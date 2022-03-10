3b0ece30858b3531028d08fe68977c8a542099f5
Kai Pan <khaiphn41@gmail.com>
2022-03-10 01:16:18 -0800

    Basic footer added. Still need to customize it but the generic layout is there.

---
+++ components/Footer/Footer.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Link from 'next/link';
+
+const Box = styled.div`
+  padding: 80px 60px;
+  background: black;
+  bottom: 0;
+  width: 100%;
+  position: fixed;
+
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 1000px;
+  margin: 0 auto;
+  /* background: red; */
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
+
+const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
+  grid-gap: 20px;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+const FooterLink = styled.a`
+  color: #fff;
+  margin-bottom: 20px;
+  font-size: 18px;
+  text-decoration: none;
+
+  &:hover {
+    color: green;
+    transition: 200ms ease-in;
+    cursor: pointer;
+  }
+`;
+
+const Heading = styled.p`
+  font-size: 24px;
+  color: #fff;
+  margin-bottom: 40px;
+  font-weight: bold;
+`;
+
+function Footer() {
+  return (
+    <Box>
+      <h1 style={{ color: "green", textAlign: "center", marginTop: "-50px" }}>
+        Robbing Hood: Commission-free Stock Viewing App
+      </h1>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <Link href="#"><FooterLink>Aim</FooterLink></Link>
+            <Link href="#"><FooterLink>Vision</FooterLink></Link>
+            <Link href="#"><FooterLink>Testimonials</FooterLink></Link>
+          </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <Link href="#"><FooterLink>Writing</FooterLink></Link>
+            <Link href="#"><FooterLink>Internships</FooterLink></Link>
+            <Link href="#"><FooterLink>Coding</FooterLink></Link>
+            <Link href="#"><FooterLink>Teaching</FooterLink></Link>
+          </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <Link href="#"><FooterLink>Khai Phan</FooterLink></Link>
+            <Link href="#"><FooterLink>Scot Rein</FooterLink></Link>
+            <Link href="#"><FooterLink>Nelson Van der Lindt</FooterLink></Link>
+          </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <Link href="#"><FooterLink>
+              <i className="fab fa-facebook-f">
+                <span style={{ marginLeft: "10px" }}>Facebook</span>
+              </i>
+            </FooterLink></Link>
+            <Link href="#"><FooterLink>
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>Instagram</span>
+              </i>
+            </FooterLink></Link>
+            <Link href="#"><FooterLink>
+              <i className="fab fa-twitter">
+                <span style={{ marginLeft: "10px" }}>Twitter</span>
+              </i>
+            </FooterLink></Link>
+            <Link href="#"><FooterLink>
+              <i className="fab fa-youtube">
+                <span style={{ marginLeft: "10px" }}>Youtube</span>
+              </i>
+            </FooterLink></Link>
+          </Column>
+        </Row>
+      </Container>
+    </Box>
+  );
+}
+
+export default Footer;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar/Navbar";
-
+import Footer from "../components/Footer/Footer";
 
 export default function Dashboard() {
     return (
         <div>
             <Navbar />
             <p>Bruh Moment LMAO</p>
+            <Footer />
         </div>
     );
 }
