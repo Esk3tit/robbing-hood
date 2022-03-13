c77d70beabf903538e894e9a377fe4fd99442e29
Kai Pan <khaiphn41@gmail.com>
2022-03-12 16:12:42 -0800

    Refactored useSWR a little to use global fetcher for now.

--- hooks/useStockNews.js
+++ hooks/useStockNews.js
@@ -1,14 +1,8 @@
 import useSWR from 'swr';
 
-const fetcher = async (...args) => {
-    const res = await fetch(...args);
-    return await res.json();
-}
-
 function useStockNews(category) {
     const { data, error } = useSWR(
-        `https://finnhub.io/api/v1/news?category=${category}&token=${process.env.FINNHUB_API_KEY_1}`,
-        fetcher
+        `${process.env.FINNHUB_API_BASE_URL}/news?category=${category}&token=${process.env.FINNHUB_API_KEY_1}`
     );
 
     return {
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '../components/Layout';
+import { SWRConfig } from 'swr';
+
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+  return await res.json();
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <SWRConfig value={{ fetcher }}>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </SWRConfig>
+    
   )
 }
 
